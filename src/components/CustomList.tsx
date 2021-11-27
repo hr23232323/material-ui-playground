cdaef49568441fbac82953938863d231df579b88
Harsh Rana <hr23232323@gmail.com>
2021-11-27 14:00:24 -0500

    added a list to the table of contents

---
+++ src/components/CustomList.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { Box } from "@mui/system";
+import { Typography } from "@mui/material";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+
+export function CustomList() {
+  return (
+    <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+      <nav aria-label="main mailbox folders">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText primary="Inbox" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText primary="Drafts" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+    </Box>
+  );
+}
--- src/components/PlaygroundContents.tsx
+++ src/components/PlaygroundContents.tsx
@@ -3,10 +3,13 @@ import React from "react";
 import { Box } from "@mui/system";
 import { Typography } from "@mui/material";
 
+import { CustomList } from "./CustomList";
+
 export function PlaygroundContents() {
   return (
-    <Box sx={{ display: "flex", m: 2 }}>
+    <Box sx={{ display: "flex", m: 2, flexDirection: "column" }}>
       <Typography variant="h3">List of Contents</Typography>
+      <CustomList />
     </Box>
   );
 }
